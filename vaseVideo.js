d85807947eb3f4bca7f23ef6e7e673ed143e17be
leylibd <leyli.bunyadzade@live.com>
2023-06-08 16:34:50 +0200

    fixed: vase page paddings and margins; added: video with autoplay

---
+++ vaseVideo.js
@@ -0,0 +1,23 @@
+console.log("test hello");
+var video = document.getElementById("img-video");
+var videoContainer = document.getElementById("video-container");
+
+var observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5
+};
+
+var observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+function handleIntersection(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+}
+
+observer.observe(videoContainer);
